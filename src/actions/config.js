8f921d9e889d80cf7ef99e807d45cee9f0c7168c
Drew Smith <andrew.j.smith.jr@gmail.com>
2017-10-10 18:48:37 -0500

    using https

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,2 @@
 export const headers = { 'Authorization': 'DrewRules' }
-export const serverUrl = 'http://readables-api-drew.herokuapp.com'
+export const serverUrl = 'https://readables-api-drew.herokuapp.com'
